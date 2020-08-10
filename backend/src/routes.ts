d0c658c3cbcd69db1debf457d88a7d29cd37b980
Daniel Filipe Schwingel <danielfilipeschwingel@gmail.com>
2020-08-10 17:46:41 -0300

    Initial commit

---
+++ backend/src/routes.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import UserController from './controllers/UserController';
+
+const routes = express.Router();
+
+routes.get('/', UserController.index);
+
+export default routes;
